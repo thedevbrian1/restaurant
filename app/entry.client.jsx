4d100d7623c8dce641c36c6a781fb2dce56cf5f4
thedevbrian1 <brayomwas95@gmail.com>
2022-11-06 19:49:48 +0300

    fggg

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -1,4 +1,4 @@
 import { RemixBrowser } from "@remix-run/react";
-import { hydrateRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom";
 
 hydrateRoot(document, <RemixBrowser />);
